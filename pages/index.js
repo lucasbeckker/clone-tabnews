9d39016b98fc1811ed09a041090afbe959686e95
lucasbeckker <becker.bl@outlook.com>
2024-12-14 21:46:28 +0000

    adicionado scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home (){
-    return <h1>Te amo minha pequena! Me manda um beijinho quando ver essa mensagem! 😘</h1>
+function Home() {
+  return (
+    <h1>
+      Te amo minha pequena! Me manda um beijinho quando ver essa mensagem! 😘
+    </h1>
+  );
 }
 
 export default Home;
